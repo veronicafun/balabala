3ad7dc4bf38ddf5f38cc22b2c3dd74ad2a60ff11
veronica <247322954@qq.com>
2018-03-28 15:00:02 +0800

    Initalize repository

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  # 指明xxx
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# root 'application#home'
+end
