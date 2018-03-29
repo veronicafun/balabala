28d9bf2e5806217899dad66e359e194f3a74a58f
veronica <247322954@qq.com>
2018-03-29 13:58:46 +0800

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
+# 这里作用是当他没有明确标题时我们用这个辅助成标题
 module ApplicationHelper
+
+def full_title(page_title = '')
+base_title = "Ruby on Rails Tutorial Sample App"
+if page_title.empty?
+  base_title
+else
+  page_title + "|" + base_title
+end
+end
 end
